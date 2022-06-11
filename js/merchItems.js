6d8bf76c515f2d2f595131ed5f3922ceddfe0fe3
SaracensFan <matthewclifford@hotmail.co.uk>
2022-06-11 19:52:49 +0100

    update

--- js/merchItems.js
+++ js/merchItems.js
@@ -9,11 +9,11 @@ export default class shopItem{
     }
 }
 
-const tshirt = new shopItem("T-shirt", 1234, 14.99, 'tshirt.jpg', null, 'Lightweight t-shirt, designed to keep you looking and feeling cool')
-const cap = new shopItem("Baseball Cap", 9876, 8.99, 'cap.jpg', null, 'Keep cool on sunny days with this really cool baseball cap.')
-const hoodie = new shopItem("Hoodie", 567019, 20.99, 'hoodie.jpg', null)
-const bag = new shopItem("Bag", 43319, 12.49, 'bag.jpg', null)
-const coffeeCup = new shopItem("Coffee Cup", 644354, 6.49, 'coffeeCup.jpg', null)
-const pencilCase = new shopItem("Pencil Case", 9834554, 3.99, 'pencilcase.jpg', null)
+const tshirt = new shopItem("T-shirt", 1234, 14.99, 't_shirt.png', null, 'Lightweight t-shirt, designed to keep you looking and feeling cool')
+const cap = new shopItem("Baseball Cap", 9876, 8.99, 'cap.png', null, 'Keep cool on sunny days with this really cool baseball cap.')
+const hoodie = new shopItem("Hoodie", 567019, 20.99, 'hoodie.png', null)
+const bag = new shopItem("Bag", 43319, 12.49, 'backpack.png', null)
+const coffeeCup = new shopItem("Coffee Cup", 644354, 6.49, 'cup.png', null)
+const pencilCase = new shopItem("Pencil Case", 9834554, 3.99, 'pencil_case.png', null)
 
 export const itemsList = [tshirt, cap, hoodie, bag, coffeeCup, pencilCase]
